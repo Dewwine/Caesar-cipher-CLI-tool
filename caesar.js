88261be8bea533c0623d65a193c0096b310039b1
Dewwine <anton22.ab@gmail.com>
2021-05-09 18:17:31 +0300

    Added Caesar cipher

---
+++ caesar.js
@@ -0,0 +1,33 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const ACTION = 'encode';
+
+const cryptAction = (text, mode = ACTION, shift) => {
+  if (mode != ACTION) {
+    shift *= -1;
+  }
+
+  return text
+    .split('')
+    .map((item) => {
+      const lowerCaseItem = item.toLowerCase();
+      const isLowerCase = item === lowerCaseItem;
+      let index = alphabet.indexOf(lowerCaseItem);
+
+      if (index >= 0) {
+        index += shift % alphabet.length;
+
+        if (index >= alphabet.length) {
+          index -= alphabet.length;
+        }
+        if (index < 0) {
+          index += alphabet.length;
+        }
+        item = alphabet[index];
+      }
+
+      return isLowerCase ? item : item.toUpperCase();
+    })
+    .join('');
+};
+
+module.exports = { cryptAction };
